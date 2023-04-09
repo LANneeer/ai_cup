0f19b02d3bfffee6117acc320bf95aa49406cca0
LANneeer <0penguin@mail.ru>
2023-04-09 07:13:03 +0600

    add cohere and openai

--- hackathon/ai/apps.py
+++ hackathon/ai/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
+import os
+import cohere
+import openai
 
 
 class AiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ai'
+    co_api_key = os.getenv('COHERE_API_KEY')
+    oai_api_key = os.getenv("OPENAI_API_KEY")
+    openai = openai
+    openai.api_key = oai_api_key
+    co = cohere.Client(api_key=co_api_key)
+
--- hackathon/ai/views.py
+++ hackathon/ai/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import ListView, TemplateView
+from ai.apps import AiConfig
 
+
+class AiView(TemplateView):
+    def get(self, request, prompt='Nothing...', *args, **kwargs):
+        prompt = prompt
+        response = AiConfig.co.generate(
+            model='command-xlarge-nightly',
+            prompt=prompt,
+            max_tokens=40,
+            temperature=0.6,
+            stop_sequences=["--"]
+        )
+        return HttpResponse(response)
 # Create your views here.
